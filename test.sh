a82c7e224fb461f7ed156da0ced0d0ea7203715d
erick <egmartinez@fi.uba.ar>
2025-05-12 15:44:55 -0300

    wip bash script

--- test.sh
+++ test.sh
@@ -20,10 +20,43 @@ verify_structure(){
 
 process_modified_file(){
     local file="$1"
+    local file_name=$(basename "$file")
     echo "Archivo modificado: $file"
     #course id se obtiene de la carpeta madre
-    local course_id=$(basename "$(dirname "$file")")
+    local course_id=$(basename "$(dirname "$(dirname "$file")")")
     echo "Enviando $file a la API del curso $course_id"
+    case "$file_name" in
+        activity_metadata.json)
+            echo "Enviando archivo $file_name..."
+            # TODO: Llamar a la api
+            ;;
+        io_test.json)
+            echo "Enviando archivo $file_name..."
+            # TODO: Llamar a la api
+            ;;
+        *.json)
+            echo -e "\033[0;31mArchivo .json no sportado: $file, debe ser activity_metadata.json o io_test.json\033[0;31m" >&2
+            ;;
+        *.c)
+            echo "Enviando archivo $file_name..."
+            # TODO: Llamar a la api
+            ;;
+        *.py)
+            echo "Enviando archivo $file_name..."
+            # TODO: Llamar a la api
+            ;;
+        *.go)
+            echo "Enviando archivo $file_name..."
+            # TODO: Llamar a la api
+            ;;
+        
+        *)
+            echo -e "\033[0;31mTipo de archivo no soportado: $file, debe ser .json, .c, .py o .go\033[0m" >&2
+            ;;
+    esac
+    
+
+
 }
 
 # Verifica si se pasó un argumento
